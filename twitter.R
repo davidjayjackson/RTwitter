8e0bd86898e3133681b3205ea4409b2fc68d0998
davidjayjackson <davidjayjackson@gmail.com>
2019-03-16 19:54:07 -0400

    fixed twitter connect statements.

--- twitter.R
+++ twitter.R
@@ -4,11 +4,10 @@ library(tm)
 library(wordcloud)
 
 consumer_kay <- "7X1wE3SrzfEyregXm9v6eMrdO"
-consumer_secret <- "9nX1GbUBef1Z8nfpYTwpsHXdF7P1sz19RvP4xSZpJoRZUGfgHW "
+consumer_secret <- "9nX1GbUBef1Z8nfpYTwpsHXdF7P1sz19RvP4xSZpJoRZUGfgHW"
 access_token <- "1106596852403716096-00rfRhADfZwOgRweOb33iTo0FFHPYD"
-access_secret <- "kXf4mW208IK2oZZc17EmBx5kuHPTgqCXfxbDKVDXYSEfK "
+access_secret <- "kXf4mW208IK2oZZc17EmBx5kuHPTgqCXfxbDKVDXYSEfK"
 #
-setup_twitter_oauth(consumer_kay,consumer_secret,
-                    access_token,access_secret)
+setup_twitter_oauth(consumer_kay,consumer_secret,access_token,access_secret)
 # Search examples from function help file
 himearch <- searchTwitter()
