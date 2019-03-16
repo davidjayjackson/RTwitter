81194114410a2f2988e67befe66111fa0282ae88
davidjayjackson <davidjayjackson@gmail.com>
2019-03-16 19:48:13 -0400

    Basic library and access info for twitter dev account.

---
+++ twitter.R
@@ -0,0 +1,14 @@
+library(twitteR)
+library(RCurl)
+library(tm)
+library(wordcloud)
+
+consumer_kay <- "7X1wE3SrzfEyregXm9v6eMrdO"
+consumer_secret <- "9nX1GbUBef1Z8nfpYTwpsHXdF7P1sz19RvP4xSZpJoRZUGfgHW "
+access_token <- "1106596852403716096-00rfRhADfZwOgRweOb33iTo0FFHPYD"
+access_secret <- "kXf4mW208IK2oZZc17EmBx5kuHPTgqCXfxbDKVDXYSEfK "
+#
+setup_twitter_oauth(consumer_kay,consumer_secret,
+                    access_token,access_secret)
+# Search examples from function help file
+himearch <- searchTwitter()
