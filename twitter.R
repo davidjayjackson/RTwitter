de121adb48a09d1a3c4aa88ac86a5a2982038c98
davidjayjackson <davidjayjackson@gmail.com>
2019-03-16 21:28:31 -0400

    Cleanin functions didn't work. But basic wordcloud does.

--- twitter.R
+++ twitter.R
@@ -2,7 +2,9 @@ library(twitteR)
 library(RCurl)
 library(tm)
 library(wordcloud)
-
+library(data.table)
+#
+rm(list=ls())
 consumer_kay <- "7X1wE3SrzfEyregXm9v6eMrdO"
 consumer_secret <- "9nX1GbUBef1Z8nfpYTwpsHXdF7P1sz19RvP4xSZpJoRZUGfgHW"
 access_token <- "1106596852403716096-00rfRhADfZwOgRweOb33iTo0FFHPYD"
@@ -10,4 +12,25 @@ access_secret <- "kXf4mW208IK2oZZc17EmBx5kuHPTgqCXfxbDKVDXYSEfK"
 #
 setup_twitter_oauth(consumer_kay,consumer_secret,access_token,access_secret)
 # Search examples from function help file
-himearch <- searchTwitter()
+himearch <- searchTwitter("solar+cme",lang="en",n=500,resultType = "recent")
+himearch_text <- sapply(himearch,function(x) x$getText())
+str(himearch_text)
+head(himearch_text)
+#
+him_corpus <- Corpus(VectorSource(himearch_text))
+him_corpus
+inspect(him_corpus)
+#
+# Lower cases, remove numbers, cut out stop words, 
+# remove punctuation, strip whitespace
+#
+him_clean <- tm_map(him_corpus,removePunctuation)
+him_clean <- tm_map(him_clean,content_transformer(tolower))
+him_clean <- tm_map(him_clean,removeWords,stopwords("english"))
+him_clean <- tm_map(him_clean,removeNumbers)
+him_clean <- tm_map(him_clean,stripWhitespace)
+head(inspect(him_corpus))
+#
+# Remove some words
+#
+wordcloud(him_clean)
